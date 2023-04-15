f6e6d9d6a8aea21c96f721c811766f32cf2b2c98
Rebecca Neves Santos <rneves-s@c2r3s1.42lisboa.com>
2023-04-15 21:14:54 +0100

    alter push swap

--- push_swap/actions/swap.c
+++ push_swap/actions/swap.c
@@ -1,31 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   swap.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rneves-s <rneves-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/15 18:31:41 by rneves-s          #+#    #+#             */
-/*   Updated: 2023/04/15 19:07:15 by rneves-s         ###   ########.fr       */
+/*   Created: 2023/04/15 18:48:11 by rneves-s          #+#    #+#             */
+/*   Updated: 2023/04/15 18:48:14 by rneves-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-
-# include<stdlib.h>
-
-typedef struct s_stack
-{
-	void			*content;
-	struct s_stack	*next;	
-}	t_list;
-
-typedef struct s_split_next
-{
-	size_t	start;
-	size_t	lenght;
-
-}	t_split_next;
-
-#endif
+#include "push_swap.h"
